3bd9a45b7a8bd77b2b3d5dfe4923194a3580b991
pachogh <139962751+pachogh@users.noreply.github.com>
2023-07-19 12:54:53 +0200

    Update streamlit_app.py
    meto iconos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-streamlit.title('Mi primera prueba')
+streamlit.title('Menu Saludable')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
