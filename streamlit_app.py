b7ce73fb0ced3c908422befb3959cd96b5113ec3
pachogh <139962751+pachogh@users.noreply.github.com>
2023-07-19 12:51:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 streamlit.title('Mi primera prueba')
 
 streamlit.header('Cabecera prueba')
+streamlit.text('linea prueba 1')
+streamlit.text('linea prueba 2')
+streamlit.text('linea prueba 3')
